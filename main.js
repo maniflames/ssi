2f0e55e94d2f103c2f3293e70f9278719197e201
maniflames <imani.dap@gmail.com>
2021-04-18 15:53:37 +0200

    initial 2

---
+++ main.js
@@ -0,0 +1,40 @@
+import io from 'socket.io/client-dist/socket.io.min.js'
+
+let acc = {}
+let socket
+
+async function requestOrientationPermission() {
+    if(typeof DeviceOrientationEvent.requestPermission == 'function') {
+        const state = await DeviceOrientationEvent.requestPermission()
+        if(state != 'granted') {
+            document.querySelector('.dialog').innerHTML = 'granting permissions is required if you want to parttake in this experience'
+            return
+        }
+    }
+
+    window.addEventListener('deviceorientation', (e) => {
+        document.querySelector('.x span').textContent = e.beta
+        document.querySelector('.y span').textContent = e.gamma
+        document.querySelector('.z span').textContent = e.alpha
+
+        acc.x = e.beta
+        acc.y = e.gamma
+        acc.z = e.alpha
+
+        socket.emit('REQ_UPDATE_INPUTS', acc)
+    })
+}
+
+window.addEventListener('load', () => {
+    socket = io('http://localhost:8001')
+    socket.on('connect', () => {
+        console.log('Connected to websocket server')
+    })
+
+    document.addEventListener('click', (e) => {
+        const target = e.target
+        if(target.nodeName == 'BUTTON') {
+            requestOrientationPermission()
+        }
+    })
+})
---
+++ server.js
@@ -0,0 +1,103 @@
+import * as net from 'net'
+import * as fs from 'fs'
+import * as https from 'https'
+import * as osc from 'osc'
+import { Server } from 'socket.io'
+
+const TCP_PORT = process.env.TCP_PORT || 8000
+const WS_PORT = process.env.WS_PORT || 8001
+
+const wsServerConfig = {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    }
+}
+
+let httpsServer
+let wsServer
+
+const certPath = '/etc/letsencrypt/live/ssi6.imanidap.nl/fullchain.pem'
+const keyPath = '/etc/letsencrypt/live/ssi6.imanidap.nl/privkey.pem'
+
+const isCertPresent = fs.existsSync(certPath)
+const isKeyPresent = fs.existsSync(keyPath)
+
+if(isCertPresent && isKeyPresent) {
+    wsServerConfig.cert = fs.readFileSync(certPath)
+    wsServerConfig.key = fs.readFileSync(keyPath)
+    httpsServer = https.createServer(wsServerConfig)
+    wsServer = new Server(httpsServer, wsServerConfig)
+    console.log("SSL detected")
+} else {
+    wsServer = new Server(WS_PORT, wsServerConfig)
+    console.log(`WebSocket server listening on port ${WS_PORT}`)
+}
+
+const server = net.createServer()
+
+let TCPConnections = []
+let UIInputs = {}
+
+server.on('connection', (socket) => {
+    console.log('new tcp client connected')
+    const fd = socket._parent ? socket._parent._handle.fd : socket._handle.fd
+
+    const OSCManager = new osc.default.TCPSocketPort({
+        socket: socket
+    })
+
+    TCPConnections.push({
+        fd: fd,
+        OSCManager: OSCManager,
+        socket: socket,
+    })
+
+    OSCManager.on('error', (err) => {
+        console.error(err)
+    })
+
+    OSCManager.on('message', (msg) => {
+        console.log(msg)
+    })
+
+    socket.on('end', () => {
+        console.log(`TCP client ${fd} disconnected`)
+        TCPConnections = TCPConnections.filter((connection) => {
+            return connection.fd != fd
+        })
+    })
+})
+
+server.listen(TCP_PORT, () => {
+    console.log(`TCP server listening on port ${TCP_PORT}`)
+})
+
+wsServer.on('connection', (socket) => {
+    console.log('new websocket client connected')
+
+    socket.on('REQ_UPDATE_INPUTS', (data) => {
+        UIInputs = data
+
+        for(let connection of TCPConnections) {
+            connection.OSCManager.send({
+                address: '/web/small',
+                args: [UIInputs.small.y]
+            })
+            connection.OSCManager.send({
+                address: '/web/medium',
+                args: [UIInputs.medium.y]
+            })
+            connection.OSCManager.send({
+                address: '/web/large',
+                args: [UIInputs.large.y]
+            })
+        }
+    })
+})
+
+if(httpsServer) {
+    httpsServer.listen(WS_PORT, () => {
+        console.log(`WS server listening on port ${WS_PORT}`)
+    })
+}
